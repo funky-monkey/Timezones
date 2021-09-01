e3c81479a2e80a53c62c5c4badceaf5722fbec12
Sidney de Koning <sidney.dekoning@gmail.com>
2021-09-01 11:04:35 +0200

    Fixed pod issue and bumped macOS version in plist

--- Timezones/Utils/Foundation/Collection+Utils.swift
+++ Timezones/Utils/Foundation/Collection+Utils.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public extension Collection {
 
-    public subscript(safe index: Index) -> Element? {
+    subscript(safe index: Index) -> Element? {
 
         return indices.contains(index) ? self[index] : nil
     }
